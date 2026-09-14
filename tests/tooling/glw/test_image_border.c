Buksa/movian

commit 1d84a9a2c42791cfc5dfbc3b462739c5d796c2ab

glw: Add support for setting margin on all widgets

src/ui/glw/glw_image.c
@@ -38,14 +38,8 @@ typedef struct glw_image {
 
   int16_t gi_border[4];
   int16_t gi_padding[4];
-  int16_t gi_margin[4];
 
-  int16_t gi_automargin_left;
-  int16_t gi_automargin_right;
-  int16_t gi_automargin_top;
-  int16_t gi_automargin_bottom;
-
-  // gi_box_ is (gi_border_ + gi_padding_ + gi_margin_)
+  // gi_box_ is (gi_border_ + gi_padding_ )
   int16_t gi_box_left;
   int16_t gi_box_right;
   int16_t gi_box_top;
@@ -235,10 +229,10 @@ render_child_autocentered(glw_image_t *gi, const glw_rctx_t *rc)
   rc0 = *rc;
 
   glw_reposition(&rc0,
-		 gi->gi_box_left + gi->gi_automargin_left,
-		 rc->rc_height - gi->gi_box_top - gi->gi_automargin_top,
-		 rc->rc_width  - gi->gi_box_right - gi->gi_automargin_right,
-		 gi->gi_box_bottom + gi->gi_automargin_bottom);
+		 gi->gi_box_left,
+		 rc->rc_height - gi->gi_box_top,
+		 rc->rc_width  - gi->gi_box_right,
+		 gi->gi_box_bottom);
 
   rc0.rc_alpha *= gi->w.glw_alpha;
   glw_render0(c, &rc0);
@@ -312,11 +306,11 @@ glw_image_render(glw_t *w, const glw_rctx_t *rc)
           ys = rc->rc_height;
 
         extra_y_margin = MAX(rc->rc_height - ys, 0) / 2;
+        glw_reposition(&rc0, 0,
+                       rc0.rc_height - extra_y_margin,
+                       rc0.rc_width,
+                       extra_y_margin);
       }
-      glw_reposition(&rc0, gi->gi_margin[0],
-                     rc0.rc_height - gi->gi_margin[1] - extra_y_margin,
-                     rc0.rc_width - gi->gi_margin[2],
-                     gi->gi_margin[3] + extra_y_margin);
       glw_scale_to_aspect(&rc0, glt->glt_aspect);
     }
     if(gi->gi_angle != 0)
@@ -394,85 +388,8 @@ glw_image_layout_tesselated(glw_root_t *gr, const glw_rctx_t *rc,
   float tex[4][2];
   float vex[4][2];
 
-  int x, y, i = 0, BL, BR;
+  int x, y, i = 0;
 
-  if(gi->w.glw_flags2 & GLW2_AUTOMARGIN) {
-
-    gi->gi_automargin_left = 0;
-    gi->gi_automargin_top = 0;
-    gi->gi_automargin_right = 0;
-    gi->gi_automargin_bottom = 0;
-
-    glw_t *c = TAILQ_FIRST(&gi->w.glw_childs);
-
-    if(c != NULL && (c->glw_flags & (GLW_CONSTRAINT_X | GLW_CONSTRAINT_Y)) ==
-		     (GLW_CONSTRAINT_X | GLW_CONSTRAINT_Y)) {
-
-      int hspill = rc->rc_width - c->glw_req_size_x -
-	gi->gi_box_left - gi->gi_box_right;
-
-      if(hspill > 0) {
-	gi->gi_automargin_left = hspill / 2 + (hspill & 1);
-	gi->gi_automargin_right = hspill / 2;
-      }
-
-      int vspill = rc->rc_height - c->glw_req_size_y -
-	gi->gi_box_top - gi->gi_box_bottom;
-
-      if(vspill > 0) {
-	gi->gi_automargin_top = vspill / 2 + (vspill & 1);
-	gi->gi_automargin_bottom = vspill / 2;
-      }
-
-    } else if(c != NULL && c->glw_flags & GLW_CONSTRAINT_W &&
-	      c->glw_req_weight < 0) {
-      float aspect = -c->glw_req_weight;
-
-      float cw = rc->rc_width - gi->gi_box_left - gi->gi_box_right;
-      float ch = rc->rc_height - gi->gi_box_top - gi->gi_box_bottom;
-
-      float myaspect = cw / ch;
-
-      if(myaspect > aspect) {
-	// We are wider than our child wants
-
-	int cwidth = ch * aspect;
-	int hspill = cw - cwidth;
-
-	if(hspill > 0) {
-	  gi->gi_automargin_left = hspill / 2 + (hspill & 1);
-	  gi->gi_automargin_right = hspill / 2;
-	}
-      } else {
-	int cheight = cw / aspect;
-	int vspill = ch - cheight;
-
-	if(vspill > 0) {
-	  gi->gi_automargin_top = vspill / 2 + (vspill & 1);
-	  gi->gi_automargin_bottom = vspill / 2;
-	}
-      }
-    } else if(gi->gi_child_aspect > 0) {
-
-      int px = rc->rc_height * gi->gi_child_aspect;
-      int hspill = rc->rc_width - px - gi->gi_box_left - gi->gi_box_right;
-
-      if(hspill > 0) {
-	gi->gi_automargin_left = hspill / 2 + (hspill & 1);
-	gi->gi_automargin_right = hspill / 2;
-      }
-
-    }
-    if(gi->w.glw_flags2 & GLW2_DEBUG)
-      printf("Automargin %d,%d,%d,%d\n",
-	     gi->gi_automargin_left,
-	     gi->gi_automargin_top,
-	     gi->gi_automargin_right,
-	     gi->gi_automargin_bottom);
-
-
-    update_box(gi);
-  }
 
   tex[1][0] = 0.0f + (float)gi->gi_border[0]  / glt->glt_xs;
   tex[2][0] = glt->glt_s - (float)gi->gi_border[2] / glt->glt_xs;
@@ -484,19 +401,16 @@ glw_image_layout_tesselated(glw_root_t *gr, const glw_rctx_t *rc,
   tex[2][1] = glt->glt_t - (float)gi->gi_border[3] / glt->glt_ys;
   tex[3][1] = glt->glt_t;
 
-  BL = gi->gi_border[0];
-  BR = gi->gi_border[2];
 
+  vex[0][0] = -1.0;
+  vex[1][0] = GLW_MIN(-1.0f + 2.0f * gi->gi_border[0] / rc->rc_width, 0.0f);
+  vex[2][0] = GLW_MAX( 1.0f - 2.0f * gi->gi_border[2] / rc->rc_width, 0.0f);
+  vex[3][0] = 1.0;
 
-  vex[0][0] = GLW_MIN(-1.0f + 2.0f * (gi->gi_margin[0] + gi->gi_automargin_left)  / rc->rc_width, 0.0f);
-  vex[1][0] = GLW_MIN(-1.0f + 2.0f * (BL + gi->gi_margin[0] + gi->gi_automargin_left)  / rc->rc_width, 0.0f);
-  vex[2][0] = GLW_MAX( 1.0f - 2.0f * (BR + gi->gi_margin[2] + gi->gi_automargin_right) / rc->rc_width, 0.0f);
-  vex[3][0] = GLW_MAX( 1.0f - 2.0f * (gi->gi_margin[2] + gi->gi_automargin_right) / rc->rc_width, 0.0f);
-
-  vex[0][1] = GLW_MAX( 1.0f - 2.0f * (gi->gi_margin[1] + gi->gi_automargin_top)  / rc->rc_height, 0.0f);
-  vex[1][1] = GLW_MAX( 1.0f - 2.0f * (gi->gi_border[1] + gi->gi_margin[1] + gi->gi_automargin_top)  / rc->rc_height, 0.0f);
-  vex[2][1] = GLW_MIN(-1.0f + 2.0f * (gi->gi_border[3] + gi->gi_margin[3] + gi->gi_automargin_bottom) / rc->rc_height, 0.0f);
-  vex[3][1] = GLW_MIN(-1.0f + 2.0f * (gi->gi_margin[3] + gi->gi_automargin_bottom) / rc->rc_height, 0.0f);
+  vex[0][1] = -1.0f;
+  vex[1][1] = GLW_MAX( 1.0f - 2.0f * gi->gi_border[1] / rc->rc_height, 0.0f);
+  vex[2][1] = GLW_MIN(-1.0f + 2.0f * gi->gi_border[3] / rc->rc_height, 0.0f);
+  vex[3][1] = 1.0f;
 
   for(y = 0; y < 4; y++) {
     for(x = 0; x < 4; x++) {
@@ -657,18 +571,15 @@ glw_image_update_constraints(glw_image_t *gi)
 
     if(c != NULL) {
       glw_set_constraints(&gi->w,
-			  c->glw_req_size_x +
+			  glw_req_width(c) +
 			  gi->gi_box_left + gi->gi_box_right,
-			  c->glw_req_size_y +
+			  glw_req_height(c) +
 			  gi->gi_box_top + gi->gi_box_bottom,
 			  c->glw_req_weight,
 			  c->glw_flags & GLW_CONSTRAINT_FLAGS);
 
-      if(gi->w.glw_flags2 & GLW2_AUTOMARGIN)
-	gi->gi_update = 1;
-
       if(gi->w.glw_flags2 & GLW2_DEBUG)
-	printf("Req size: %d,%d\n", c->glw_req_size_x, c->glw_req_size_y);
+	printf("Req size: %d,%d\n", glw_req_width(c), glw_req_height(c));
 
 
     } else if(glt != NULL) {
@@ -686,11 +597,11 @@ glw_image_update_constraints(glw_image_t *gi)
       glw_set_constraints(&gi->w, 0, 0, -glt->glt_aspect, GLW_CONSTRAINT_W);
     } else if(gi->w.glw_flags & GLW_CONSTRAINT_CONF_X) {
 
-      int ys = gi->w.glw_req_size_x / glt->glt_aspect;
+      int ys = glw_req_width(&gi->w) / glt->glt_aspect;
       glw_set_constraints(&gi->w, 0, ys, 0, GLW_CONSTRAINT_Y);
     } else if(gi->w.glw_flags & GLW_CONSTRAINT_CONF_Y) {
 
-      int xs = gi->w.glw_req_size_y * glt->glt_aspect;
+      int xs = glw_req_height(&gi->w) * glt->glt_aspect;
       glw_set_constraints(&gi->w, xs, 0, 0, GLW_CONSTRAINT_X);
     }
   }
@@ -1030,11 +941,8 @@ glw_image_layout(glw_t *w, const glw_rctx_t *rc)
 	     rc0.rc_height, (gi->gi_box_top + gi->gi_box_bottom),
 	     rc0.rc_height - (gi->gi_box_top + gi->gi_box_bottom));
 
-    rc0.rc_width  -= gi->gi_box_left + gi->gi_box_right +
-      gi->gi_automargin_left + gi->gi_automargin_right;
-    rc0.rc_height -= gi->gi_box_top  + gi->gi_box_bottom +
-      gi->gi_automargin_top + gi->gi_automargin_bottom;
-
+    rc0.rc_width  -= gi->gi_box_left + gi->gi_box_right;
+    rc0.rc_height -= gi->gi_box_top  + gi->gi_box_bottom;
 
     if(rc0.rc_height >= 0 && rc0.rc_width >= 0)
       glw_layout0(c, &rc0);
@@ -1164,10 +1072,10 @@ glw_image_set_float3(glw_t *w, glw_attribute_t attrib, const float *rgb,
 static void
 update_box(glw_image_t *gi)
 {
-  gi->gi_box_left   = gi->gi_margin[0] + gi->gi_border[0] + gi->gi_padding[0];
-  gi->gi_box_top    = gi->gi_margin[1] + gi->gi_border[1] + gi->gi_padding[1];
-  gi->gi_box_right  = gi->gi_margin[2] + gi->gi_border[2] + gi->gi_padding[2];
-  gi->gi_box_bottom = gi->gi_margin[3] + gi->gi_border[3] + gi->gi_padding[3];
+  gi->gi_box_left   = gi->gi_border[0] + gi->gi_padding[0];
+  gi->gi_box_top    = gi->gi_border[1] + gi->gi_padding[1];
+  gi->gi_box_right  = gi->gi_border[2] + gi->gi_padding[2];
+  gi->gi_box_bottom = gi->gi_border[3] + gi->gi_padding[3];
 
   if(gi->w.glw_flags2 & GLW2_DEBUG)
     printf("Box: %d,%d,%d,%d == %d,%d\n",
@@ -1198,11 +1106,6 @@ image_set_int16_4(glw_t *w, glw_attribute_t attrib, const int16_t *v,
 
   case GLW_ATTRIB_PADDING:
     if(!glw_attrib_set_int16_4(gi->gi_padding, v))
-      return 0;
-    break;
-
-  case GLW_ATTRIB_MARGIN:
-    if(!glw_attrib_set_int16_4(gi->gi_margin, v))
       return 0;
     break;
 

src/ui/glw/glw.c
@@ -459,25 +459,60 @@ glw_layout0(glw_t *w, const glw_rctx_t *rc)
       glw_signal0(w, GLW_SIGNAL_ACTIVE, NULL);
     }
   }
+
   if(unlikely(w->glw_dynamic_eval & mask))
     glw_view_eval_layout(w, rc, mask);
 
-  w->glw_class->gc_layout(w, rc);
+  if(unlikely(w->glw_flags & GLW_HAVE_MARGINS)) {
+    glw_rctx_t rc0 = *rc;
+    glw_reposition(&rc0,
+                   w->glw_margin[0],
+                   rc->rc_height - w->glw_margin[1],
+                   rc->rc_width - w->glw_margin[2],
+                   w->glw_margin[3]);
+    w->glw_class->gc_layout(w, &rc0);
+  } else {
+    w->glw_class->gc_layout(w, rc);
+  }
 }
 
 
-/**
- *
- */
 void
-glw_render_zoffset(glw_t *w, const glw_rctx_t *rc)
+glw_render0(glw_t *w, const glw_rctx_t *rc)
 {
-  glw_rctx_t rc0 = *rc;
-  int zmax = 0;
-  rc0.rc_zmax = &zmax;
-  rc0.rc_zindex = w->glw_zoffset;
-  w->glw_class->gc_render(w, &rc0);
+  if(unlikely(w->glw_zoffset)) {
+    glw_rctx_t rc0 = *rc;
+    int zmax = 0;
+    rc0.rc_zmax = &zmax;
+    rc0.rc_zindex = w->glw_zoffset;
+
+
+    if(unlikely(w->glw_flags & GLW_HAVE_MARGINS)) {
+      glw_reposition(&rc0,
+                     w->glw_margin[0],
+                     rc->rc_height - w->glw_margin[1],
+                     rc->rc_width - w->glw_margin[2],
+                     w->glw_margin[3]);
+    }
+    w->glw_class->gc_render(w, &rc0);
+
+  } else if(unlikely(w->glw_flags & GLW_HAVE_MARGINS)) {
+    glw_rctx_t rc0 = *rc;
+    glw_reposition(&rc0,
+                   w->glw_margin[0],
+                   rc->rc_height - w->glw_margin[1],
+                   rc->rc_width - w->glw_margin[2],
+                   w->glw_margin[3]);
+
+    w->glw_class->gc_render(w, &rc0);
+  } else {
+    w->glw_class->gc_render(w, rc);
+  }
+
+  if(unlikely(w->glw_flags2 & GLW2_DEBUG))
+    glw_wirebox(w->glw_root, rc);
 }
+
 
 
 /**
@@ -2675,8 +2710,8 @@ void
 glw_copy_constraints(glw_t *w, glw_t *src)
 {
   glw_set_constraints(w,
-		      src->glw_req_size_x,
-		      src->glw_req_size_y,
+		      glw_req_width(src),
+		      glw_req_height(src),
 		      src->glw_req_weight,
 		      glw_filter_constraints(src));
 }

src/ui/glw/glw.h
@@ -193,7 +193,6 @@ typedef enum {
   GLW_ATTRIB_TRANSLATION,
   GLW_ATTRIB_ROTATION,
   GLW_ATTRIB_PLANE,
-  GLW_ATTRIB_MARGIN,
   GLW_ATTRIB_BORDER,
   GLW_ATTRIB_PADDING,
   GLW_ATTRIB_FONT,
@@ -734,6 +733,11 @@ typedef struct glw_class {
   /**
    *
    */
+  void (*gc_set_margin)(struct glw *w, const int16_t *v, glw_style_t *gs);
+
+  /**
+   *
+   */
   void (*gc_freeze)(struct glw *w);
 
   /**
@@ -1173,6 +1177,9 @@ typedef struct glw {
   glw_styleset_t *glw_styles; // List of available styles
   struct glw_style_binding_list glw_style_bindings; // Bound styles
 
+  rstr_t *glw_file;
+  int glw_line;
+
   int glw_refcnt;
 
   /**
@@ -1181,6 +1188,8 @@ typedef struct glw {
   int16_t glw_req_size_x;
   int16_t glw_req_size_y;
   float glw_req_weight;
+
+  int16_t glw_margin[4];
 
   int glw_flags;
 
@@ -1207,6 +1216,8 @@ typedef struct glw {
 #define GLW_CAN_SCROLL           0x4000
 #define GLW_MARK                 0x8000
 
+#define GLW_HAVE_MARGINS         0x10000
+
 #define GLW_CLIPPED              0x1000000
 #define GLW_FHP_SPILL_TO_CHILDS  0x4000000
 
@@ -1230,7 +1241,6 @@ typedef struct glw {
 #define GLW2_SHADOW                 0x100
 #define GLW2_AUTOFADE               0x200
 #define GLW2_EXPEDITE_SUBSCRIPTIONS 0x400
-#define GLW2_AUTOMARGIN             0x800
 #define GLW2_NO_INITIAL_TRANS       0x2000
 #define GLW2_FOCUS_ON_CLICK         0x4000
 #define GLW2_AUTOREFOCUSABLE        0x8000
@@ -1257,14 +1267,24 @@ typedef struct glw {
 
   uint8_t glw_dynamic_eval;   // GLW_VIEW_EVAL_ -flags
 
-  rstr_t *glw_file;
-  int glw_line;
 } glw_t;
 
 static __inline int
 glw_filter_constraints(const glw_t *w)
 {
   return (w->glw_flags & ~w->glw_flags2) & GLW_CONSTRAINT_FLAGS;
+}
+
+static __inline int
+glw_req_width(const glw_t *w)
+{
+  return w->glw_req_size_x + w->glw_margin[0] + w->glw_margin[2];
+}
+
+static __inline int
+glw_req_height(const glw_t *w)
+{
+  return w->glw_req_size_y + w->glw_margin[1] + w->glw_margin[3];
 }
 
 #define GLW_INIT_KEYBOARD_MODE 0x1
@@ -1328,6 +1348,8 @@ void glw_set_height(glw_t *w, int v, glw_style_t *origin);
 void glw_set_align(glw_t *w, int v, glw_style_t *origin);
 
 void glw_set_hidden(glw_t *w, int v, glw_style_t *origin);
+
+void glw_set_margin(glw_t *w, const int16_t *v, glw_style_t *origin);
 
 void glw_set_divider(glw_t *w, int v);
 
@@ -1551,17 +1573,7 @@ static inline int glw_debug(glw_t *w)
   return w->glw_flags2 & GLW2_DEBUG;
 }
 
-static inline void glw_render0(glw_t *w, const glw_rctx_t *rc)
-{
-  if(unlikely(w->glw_zoffset != 0)) {
-    glw_render_zoffset(w, rc);
-  } else {
-    w->glw_class->gc_render(w, rc);
-  }
-
-  if(unlikely(w->glw_flags2 & GLW2_DEBUG))
-    glw_wirebox(w->glw_root, rc);
-}
+void glw_render0(glw_t *w, const glw_rctx_t *rc);
 
 static inline void glw_zinc(glw_rctx_t *rc)
 {

commit 593627edd73c8c24848702cee6bc992ccf4ef519

glw: the nine-slice axes do not run the same way, and one edge said they did (#254)
`glw_image_layout_tesselated` built its 4x4 vertex grid with two copies of the
same arithmetic, one per axis, and the copies disagreed about which way the
axis runs. x produced an ordered row; y produced `-1, 1-2b/h, -1+2b/h, +1`,
whose row 0 is the widget's bottom while its texture coordinate t=0 is the
image's top. Every horizontal quad built from it was inverted or degenerate,
so the top band, the bottom band and all four corners collapsed and only the
left and right survived.

Broken by 1d84a9a2c (2016-06-01, "glw: Add support for setting margin on all
widgets"), which dropped the margin terms and simplified the outer y edges to
the x convention while leaving rows 1 and 2 descending. Substituting margin=0
into the expressions it deleted yields +1 and -1 for those two lines, not -1
and +1: the x answer was copied into the y block rather than evaluated. The
texture coordinates went untouched and the sibling `glw_image_layout_alpha_edges`
kept `1.0 ... -1.0` to this day, so it was a slip, not a convention change.

`glw_image_border_axis` is that arithmetic written once and called for both
axes, with the direction passed in rather than baked into two copies. The x
row comes out identical, verified by instrumented vertex dumps on both builds.

Verified on the stand with a 32x32 probe texture whose nine slices each carry
a distinct colour, at three widget heights plus borderOnly and an asymmetric
border, and against `--skin glwskins/old` -- the only consumer of these
borders -- where the home tiles gain both horizontal bands and their corners.

Also here: the metadata artifact, whose five glw_image.c widget classes all
moved ten lines down; and a gates job for the unit test, which had no Makefile
target and no workflow step, so the invariant it claims to pin could have
broken again with every check green. That gap was found by Codex review.

Closes #117. Related: #255, #256.

.github/workflows/gates.yml
@@ -140,6 +140,29 @@ jobs:
           python3 -m unittest discover -s tests/tooling/mdev
           -p 'test_lsp_doctor.py' -v
 
+  glw-geometry:
+    name: glw geometry unit tests
+    runs-on: ubuntu-latest
+    timeout-minutes: 5
+    steps:
+      - uses: actions/checkout@v4
+        with:
+          submodules: false
+
+      # The "deliberately NOT a build" note above is about Movian's build --
+      # submodules, libav, ext-cache. It does not apply here: these tests are
+      # header-only and dependency-free by construction, so the whole step is
+      # one `cc` invocation and takes under half a second. A test nobody runs
+      # cannot pin an invariant, and the invariant this one pins -- that the
+      # nine-slice vertex rows stay ordered in their axis's own direction --
+      # is exactly the one that silently broke in 2016 and stayed broken for
+      # nine years (movian#117).
+      - name: glw_image_border_axis
+        run: |
+          cc -I src -Werror -o /tmp/test_image_border \
+            tests/tooling/glw/test_image_border.c
+          /tmp/test_image_border
+
   hygiene:
     name: whitespace and merge markers
     runs-on: ubuntu-latest

generated/movian-api-v1.d.ts
@@ -1,5 +1,5 @@
 // Generated by support/devtools/metadata/gen.py -- do not edit.
-// movianRevision: c45020df093560b960ebb5aba80e9821f9a7ccc0
+// movianRevision: 2a85de8a9cc454545f5c504e1a2c1a0498cd1e11
 //
 // apiversion 1 ONLY. src/plugins.c:712 defaults a plugin.json with
 // no `apiversion` to 1, and src/ecmascript/ecmascript.c:913 loads

generated/movian-api.d.ts
@@ -1,5 +1,5 @@
 // Generated by support/devtools/metadata/gen.py -- do not edit.
-// movianRevision: c45020df093560b960ebb5aba80e9821f9a7ccc0
+// movianRevision: 2a85de8a9cc454545f5c504e1a2c1a0498cd1e11
 // Duktape ES5.1 -- no ES6+ in plugin code.
 // Compile with --lib ES2015 (or any lib set WITHOUT dom):
 // lib.dom declares `console` and `Plugin` too, and the

src/ui/glw/glw_image.c
@@ -19,6 +19,7 @@
  */
 #include "glw.h"
 #include "glw_renderer.h"
+#include "glw_image_border.h"
 #include "glw_texture.h"
 
 typedef struct glw_image {
@@ -386,7 +387,7 @@ glw_image_layout_tesselated(glw_root_t *gr, const glw_rctx_t *rc,
                             glw_image_t *gi, glw_loadable_texture_t *glt)
 {
   float tex[4][2];
-  float vex[4][2];
+  float vex[2][4];   // [axis][row], because one helper fills one axis
 
   int x, y, i = 0;
 
@@ -402,19 +403,24 @@ glw_image_layout_tesselated(glw_root_t *gr, const glw_rctx_t *rc,
   tex[3][1] = glt->glt_t;
 
 
-  vex[0][0] = -1.0;
-  vex[1][0] = GLW_MIN(-1.0f + 2.0f * gi->gi_border[0] / rc->rc_width, 0.0f);
-  vex[2][0] = GLW_MAX( 1.0f - 2.0f * gi->gi_border[2] / rc->rc_width, 0.0f);
-  vex[3][0] = 1.0;
-
-  vex[0][1] = -1.0f;
-  vex[1][1] = GLW_MAX( 1.0f - 2.0f * gi->gi_border[1] / rc->rc_height, 0.0f);
-  vex[2][1] = GLW_MIN(-1.0f + 2.0f * gi->gi_border[3] / rc->rc_height, 0.0f);
-  vex[3][1] = 1.0f;
+  // One arithmetic for both axes, with the DIRECTION stated here because the
+  // axes do not run the same way: +1 is the widget's top and t=0 is the
+  // image's top, so y runs +1 -> -1 against tex's 0 -> glt_t.
+  //
+  // This held from the start until 1d84a9a2c (2016-06-01, "glw: Add support
+  // for setting margin on all widgets"), which dropped the margin terms and
+  // wrote the outer y edges as -1 and +1 -- the x convention -- while leaving
+  // rows 1 and 2 descending. The result, -1, 1-2b/h, -1+2b/h, +1, is not
+  // ordered, so every horizontal quad built from it is inverted or
+  // degenerate and the top and bottom bands stop drawing (movian#117).
+  glw_image_border_axis(vex[0], -1.0f, gi->gi_border[0], gi->gi_border[2],
+                         1.0f, rc->rc_width);
+  glw_image_border_axis(vex[1],  1.0f, gi->gi_border[1], gi->gi_border[3],
+                        -1.0f, rc->rc_height);
 
   for(y = 0; y < 4; y++) {
     for(x = 0; x < 4; x++) {
-      glw_renderer_vtx_pos(&gi->gi_gr, i, vex[x][0], vex[y][1], 0.0f);
+      glw_renderer_vtx_pos(&gi->gi_gr, i, vex[0][x], vex[1][y], 0.0f);
       glw_renderer_vtx_st (&gi->gi_gr, i, tex[x][0], tex[y][1]);
       i++;
     }
@@ -652,8 +658,12 @@ static uint16_t borderonlyobject[] = {
   2, 7, 3,
   8, 5, 4,
   8, 9, 5,
-  //  9, 6, 5,
-  //  9, 10, 6,
+  // No centre quad here, deliberately: vertices 5, 6, 9, 10 are rows 1-2 by
+  // columns 1-2, which IS the centre, and `borderOnly` exists to suppress
+  // it. The triangle count at the call site is the proof -- `borderobject`
+  // is initialised with 18 and this one with 16, and those two triangles are
+  // the difference. movian#117 suspected these lines of being the missing
+  // horizontal bands. They are not.
   10, 7, 6,
   10, 11, 7,
   12, 13, 8,

src/ui/glw/glw_image_border.h
@@ -1,0 +1,67 @@
+/*
+ *  Copyright (C) 2007-2015 Lonelycoder AB
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  This program is also available under a commercial proprietary license.
+ *  For more information, contact andreas@lonelycoder.com
+ */
+#pragma once
+
+/**
+ * The four coordinates of one axis of a nine-slice split: the two edges, and
+ * the two rows inset from them by their borders.
+ *
+ * The axis runs from `start` to `end`, and the CALLER states which way round
+ * that is. That is the whole point. The two axes do not run the same way: a
+ * GLW widget's +1 is its top and a texture's t=0 is the image's top
+ * (`glw_image_layout_repeated` pairs `+1` with `t=0`; the pixmap's first
+ * scanline is the image's first, libjpeg.c:102-107, uploaded to t=0 by
+ * glw_texture_opengl.c:89-95). So x runs -1 -> +1 and y runs +1 -> -1, and
+ * writing that at the call site is what keeps the texture the right way up.
+ *
+ * It was implicit before, in two copies of the arithmetic, and it got lost:
+ * a 2016 margin refactor (1d84a9a2c) replaced the y EDGES with -1 and +1 --
+ * the x convention -- and left the two inset rows computing from the old
+ * descending one. The rows then arrived out of order, every horizontal quad
+ * was degenerate or inverted, and the top and bottom bands stopped drawing
+ * while the verticals carried on (movian#117).
+ *
+ * `border_start` belongs to the `start` edge and `border_end` to the `end`
+ * edge. A border wider than half the box clamps to the middle rather than
+ * crossing over, so the result is always monotone from `start` to `end`.
+ *
+ * Deliberately dependency-free so it can be tested without the renderer:
+ * see tests/tooling/glw/test_image_border.c.
+ */
+static inline void
+glw_image_border_axis(float out[4], float start, int border_start,
+                      int border_end, float end, float extent)
+{
+  const float span = end - start;
+  const float inset_start = start + span * border_start / extent;
+  const float inset_end   = end   - span * border_end   / extent;
+
+  out[0] = start;
+  out[3] = end;
+
+  /* Neither inset may pass the middle, whichever way the axis runs. */
+  if(span > 0.0f) {
+    out[1] = inset_start < 0.0f ? inset_start : 0.0f;
+    out[2] = inset_end   > 0.0f ? inset_end   : 0.0f;
+  } else {
+    out[1] = inset_start > 0.0f ? inset_start : 0.0f;
+    out[2] = inset_end   < 0.0f ? inset_end   : 0.0f;
+  }
+}

tests/tooling/glw/test_image_border.c
@@ -1,0 +1,133 @@
+/*
+ * The nine-slice vertex split, in isolation (movian#117).
+ *
+ * A `backdrop` with `border: N` drew only its left and right bands. The
+ * horizontals never appeared, because the two axes computed the same thing
+ * with two copies of the arithmetic and the copies had diverged: x produced
+ * a monotonic row of vertex coordinates and y produced
+ * `-1, 1-2b/h, -1+2b/h, 1`, whose middle two rows are in the wrong order.
+ * Every horizontal quad built from them is degenerate or inverted, so the
+ * bands collapse. The vertical ones were fine, which is why only they showed.
+ *
+ * `glw_image_border_axis` is that arithmetic, once, called for both axes, so
+ * the two cannot drift apart again. What this file pins is the property the
+ * geometry needs and the old code lost: the four coordinates are
+ * non-decreasing. A tesselated quad between rows i and i+1 has a
+ * non-negative height exactly when that holds.
+ *
+ * Header-only and dependency-free on purpose: the real function is under
+ * test, not a copy of it, and pulling glw.h would drag the renderer in.
+ *
+ * Run by the `glw geometry unit tests` job in .github/workflows/gates.yml on
+ * every push and pull request. By hand:
+ *   cc -I src -Werror -o /tmp/t tests/tooling/glw/test_image_border.c
+ *   /tmp/t
+ */
+#include <stdio.h>
+#include <string.h>
+
+#include "ui/glw/glw_image_border.h"
+
+static int failures;
+
+static void
+check(const char *what, int ok, const char *detail)
+{
+  if(!ok) {
+    failures++;
+    printf("  FAIL %-34s %s\n", what, detail);
+  } else {
+    printf("  PASS %-34s %s\n", what, detail);
+  }
+}
+
+/* Monotone in the direction the axis was asked to run. A quad between rows
+   i and i+1 has a non-negative extent exactly when this holds, whichever way
+   round the axis is. */
+static int
+monotone(const float v[4])
+{
+  const float span = v[3] - v[0];
+  return (v[1] - v[0]) * span >= 0.0f && (v[2] - v[1]) * span >= 0.0f
+      && (v[3] - v[2]) * span >= 0.0f;
+}
+
+int
+main(void)
+{
+  char detail[160];
+  float v[4];
+
+  /* The borders and box sizes from the report: border 3/4/8 on boxes from
+     1.5em to 4.6em, which at this skin's em is roughly 24 to 74 pixels. */
+  static const int borders[] = {0, 1, 3, 4, 8, 12, 24};
+  static const float extents[] = {8.0f, 24.0f, 37.0f, 74.0f, 300.0f};
+
+  printf("every border/extent pair yields a non-decreasing row:\n");
+  for(unsigned int b = 0; b < sizeof(borders) / sizeof(borders[0]); b++) {
+    for(unsigned int e = 0; e < sizeof(extents) / sizeof(extents[0]); e++) {
+      glw_image_border_axis(v, -1.0f, borders[b], borders[b], 1.0f,
+                            extents[e]);
+      snprintf(detail, sizeof(detail),
+               "border=%d extent=%.0f -> %.3f %.3f %.3f %.3f",
+               borders[b], extents[e], v[0], v[1], v[2], v[3]);
+      check("monotone, x direction", monotone(v), detail);
+    }
+  }
+
+  printf("asymmetric borders keep their order too:\n");
+  glw_image_border_axis(v, -1.0f, 2, 10, 1.0f, 40.0f);
+  snprintf(detail, sizeof(detail), "lo=2 hi=10 -> %.3f %.3f %.3f %.3f",
+           v[0], v[1], v[2], v[3]);
+  check("monotone, lo < hi", monotone(v), detail);
+  glw_image_border_axis(v, -1.0f, 10, 2, 1.0f, 40.0f);
+  snprintf(detail, sizeof(detail), "lo=10 hi=2 -> %.3f %.3f %.3f %.3f",
+           v[0], v[1], v[2], v[3]);
+  check("monotone, lo > hi", monotone(v), detail);
+
+  printf("the band nearest each edge is the one that edge asked for:\n");
+  glw_image_border_axis(v, -1.0f, 4, 0, 1.0f, 40.0f);
+  snprintf(detail, sizeof(detail), "lo=4 hi=0 -> row1=%.3f row2=%.3f",
+           v[1], v[2]);
+  /* A border on the low edge only: row 1 moves off -1, row 2 stays at +1. */
+  check("low border moves row 1", v[1] > -1.0f && v[2] == 1.0f, detail);
+  glw_image_border_axis(v, -1.0f, 0, 4, 1.0f, 40.0f);
+  snprintf(detail, sizeof(detail), "lo=0 hi=4 -> row1=%.3f row2=%.3f",
+           v[1], v[2]);
+  check("high border moves row 2", v[1] == -1.0f && v[2] < 1.0f, detail);
+
+  printf("a border wider than the box collapses rather than inverting:\n");
+  glw_image_border_axis(v, -1.0f, 40, 40, 1.0f, 10.0f);
+  snprintf(detail, sizeof(detail), "border=40 extent=10 -> %.3f %.3f %.3f %.3f",
+           v[0], v[1], v[2], v[3]);
+  check("clamped to the centre", monotone(v) && v[1] == 0.0f
+        && v[2] == 0.0f, detail);
+
+  /* The y axis runs the other way, and this is the case a symmetric frame
+     hides. `glw_image_layout_repeated` pairs vertex +1 with texture t=0, and
+     t=0 holds the image's first scanline, so the TOP border must land at the
+     +1 end. An earlier version of this fix ran y ascending like x, which put
+     the top border's texels on the bottom band -- invisible with an 8x8
+     frame, obvious with an asymmetric one. */
+  printf("the y axis runs +1 -> -1, so the top border stays at the top:\n");
+  glw_image_border_axis(v, 1.0f, 8, 2, -1.0f, 40.0f);
+  snprintf(detail, sizeof(detail), "top=8 bottom=2 -> %.3f %.3f %.3f %.3f",
+           v[0], v[1], v[2], v[3]);
+  check("monotone, y direction", monotone(v), detail);
+  check("row 0 is the top edge", v[0] == 1.0f, detail);
+  check("row 3 is the bottom edge", v[3] == -1.0f, detail);
+  /* 8/40 of the half-height down from +1; 2/40 up from -1. Asymmetric on
+     purpose: equal borders would pass even with the two swapped. */
+  check("top inset is the bigger one",
+        (1.0f - v[1]) > (v[2] - (-1.0f)), detail);
+
+  printf("and a y border wider than the box still collapses inward:\n");
+  glw_image_border_axis(v, 1.0f, 40, 40, -1.0f, 10.0f);
+  snprintf(detail, sizeof(detail), "border=40 extent=10 -> %.3f %.3f %.3f %.3f",
+           v[0], v[1], v[2], v[3]);
+  check("clamped, y direction", monotone(v) && v[1] == 0.0f && v[2] == 0.0f,
+        detail);
+
+  printf("\n%s: %d failure(s)\n", failures ? "FAIL" : "OK", failures);
+  return failures != 0;
+}